5ccfd4a4aea201607340647d3aa034f168a9ea25
Renan1309 <renan.a.b.silva@gmail.com>
2021-01-12 22:48:26 -0300

    Adicionando modularizacao dos componentes e estudo sobre ng-content

---
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,20 @@
+import { PhotoModule } from './../photo/photo.module';
+import { NgModule } from '@angular/core';
+import { PhotoListComponent } from './photo-list.component';
+import { PhotosComponent } from './photos/photos.component';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { FilterByDescription } from './filter-by-description';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'src/app/shared/components/card/card.module';
+
+
+@NgModule({
+    declarations: [
+        PhotoListComponent,
+        PhotosComponent,
+        LoadButtonComponent,
+        FilterByDescription
+    ],
+    imports: [ CommonModule, PhotoModule, CardModule ]
+})
+export class PhotoListModule {}
